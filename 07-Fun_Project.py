59f6dfd294f0586480c8e09a89b31ddcf0aa8c06
BhuvanDakshD <bhuvandaksh2007@gmail.com>
2026-03-29 18:26:25 +0530

    07-Fun_Project.py

--- 07-Fun_Project.py
+++ 07-Fun_Project.py
@@ -1,6 +1,6 @@
 import random
 
-print("🎯 Welcome to Number Guessing Game!")
+print(" Welcome to Number Guessing Game!")
 print("Guess a number between 1 and 100")
 print("You have 7 chances!\n")
 
@@ -15,12 +15,12 @@ for i in range(chances):
     guess = int(input("Enter your guess: "))
 
     if guess < number:
-        print("📉 Too low! Try again.")
+        print(" Too low! Try again.")
     elif guess > number:
-        print("📈 Too high! Try again.")
+        print(" Too high! Try again.")
     else:
-        print("🎉 Congratulations! You guessed the correct number!")
+        print(" Congratulations! You guessed the correct number!")
         break
 else:
-    print("\n😢 You lost!")
+    print("\n You lost!")
     print("The correct number was:", number)
